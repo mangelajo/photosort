be8cdae5935c258f4409c118bc7e9d08d5e744e7
Miguel Angel Ajo Pelayo <mangelajo@redhat.com>
2015-04-23 18:20:12 +0200

    inc version for pip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
         name = 'photosort',
-        version = '2015.1',
+        version = '2015.2',
         description = 'Picture inbox simplified',
         author = 'Miguel Angel Ajo Pelayo',
         author_email = 'miguelangel@ajo.es',
