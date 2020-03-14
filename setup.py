5567abb7d3caf79b8150b9269d194a3397a8165b
Miguel Angel Ajo <majopela@redhat.com>
2020-03-14 11:28:23 +0100

    Setup tox, etc, WIP

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
-
 import setuptools
 
+
 setuptools.setup(
-        name = 'photosort',
-        version = '2018.3',
-        description = 'Picture inbox simplified',
-        author = 'Miguel Angel Ajo Pelayo',
-        author_email = 'miguelangel@ajo.es',
-        url = 'https://github.com/mangelajo/photosort',
-        packages = setuptools.find_packages(exclude = ['ez_setup']),
-        include_package_data = True,
-        zip_safe = False,
-        entry_points = {
+        name='photosort',
+        version='2018.3',
+        description='Picture inbox simplified',
+        author='Miguel Angel Ajo Pelayo',
+        author_email='miguelangel@ajo.es',
+        url='https://github.com/mangelajo/photosort',
+        packages=setuptools.find_packages(exclude=['ez_setup']),
+        include_package_data=True,
+        zip_safe=False,
+        entry_points={
             'console_scripts': [
                 'photosort = photosort.photosort:main'
             ]},
-        install_requires = ['pyaml', 'Pillow'],
-        data_files = [('etc', ['etc/photosort.yml'])],
-        test_suite = 'photosort.test.testcases'
+        install_requires=['pyaml', 'Pillow'],
+
+        data_files=[('etc', ['etc/photosort.yml'])],
+        test_suite='photosort.test.testcases'
         )
 
 
